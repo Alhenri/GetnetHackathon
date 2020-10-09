54d98014b2ea391b78c25c0abace445514247f02
Alhenri <ahssf@cin.ufpe.br>
2020-10-09 10:46:28 -0300

    Iniciando o repositório

---
+++ src/pages/PageHome/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageDefault from '../../components/PageDefault'
+
+function PageHome(){
+    return(
+        <PageDefault>
+            <h1>Essa é a home</h1>
+        </PageDefault>
+    )
+}
+
+export default PageHome;
